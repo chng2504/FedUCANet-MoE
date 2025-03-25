5a4fe67d1ed1560d395890b4da8149afe3629e4f
nexisato <978452096@qq.com>
2025-03-25 18:46:08 +0800

    feat: update fedavg

---
+++ aggregator.py
@@ -0,0 +1,43 @@
+
+from typing import List, Dict
+import torch
+from models.mvn4 import MVN4TrimNet
+from loguru import logger
+import numpy as np
+
+
+def FedAvg(weights: List[Dict[str, torch.Tensor]], alpha: List[float] ) -> Dict[str, torch.Tensor]:
+    """
+    Federated Averaging
+    weights: List[Dict[str, torch.Tensor]]: list of model weights, representing the weights of each client
+    alpha: List[float]: list of alpha, representing the importance of each client
+    """
+    assert len(weights) == len(alpha)
+
+    avg_weights = {}
+    for key in weights[0].keys():
+        avg_weights[key] = torch.sum(torch.stack([alpha[i] * weights[i][key] for i in range(len(weights))]), dim=0)
+
+    return avg_weights
+    
+    
+    
+if __name__ == "__main__":
+    model = MVN4TrimNet(num_classes=6)
+    client_num = 20
+    logger.info(f"Client number: {client_num}")
+
+    model_list = [model.state_dict() for _ in range(client_num)]
+    alpha = np.random.uniform(0, 1, client_num)
+    alpha = alpha / alpha.sum()
+    print(alpha)
+    print(alpha.sum())
+    
+    avg_weights = FedAvg(model_list, alpha)
+    # print(avg_weights)
+    
+    new_model = MVN4TrimNet(num_classes=6)
+    new_model.load_state_dict(avg_weights)
+    print(new_model)
+    
+    
